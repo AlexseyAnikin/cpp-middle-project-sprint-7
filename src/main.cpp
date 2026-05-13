595e86dcb3876e880588a84e69e3fe1b816b616e
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-05-13 20:57:27 +0300

    feat: реализовал асинхронный HTTP proxy сервер

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,16 @@
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/detached.hpp>
+#include <boost/asio/read.hpp>
 #include <boost/asio/read_until.hpp>
+#include <boost/asio/write.hpp>
 #include <boost/asio/use_awaitable.hpp>
 
 #include <string_view>
 #include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+#include <string>
 
 using boost::asio::io_service;
 using boost::asio::co_spawn;
@@ -27,7 +32,121 @@ constexpr std::string_view delimiter = "\r\n\r\n";
 
 awaitable<void> session(tcp::socket client_socket, io_service& io_service)
 {
-  // code here
+  try
+  {
+    std::string client_request;
+
+    co_await boost::asio::async_read_until(
+      client_socket,
+      dynamic_buffer(client_request),
+      delimiter,
+      use_awaitable
+    );
+
+    auto [host, port] = findHostPort(client_request);
+
+    std::cout << "Client requested host: " << host
+              << ", port: " << port << std::endl;
+
+    tcp::resolver resolver(io_service);
+
+    auto endpoints = co_await resolver.async_resolve(
+      host, port, use_awaitable
+    );
+
+    tcp::socket server_socket(io_service);
+
+    co_await boost::asio::async_connect(
+      server_socket, endpoints, use_awaitable
+    );
+
+    co_await boost::asio::async_connect(
+      server_socket, buffer(client_request), use_awaitable
+    );
+
+    std::string server_response;
+
+    co_await boost::asio::async_read_until(
+      server_socket, dynamic_buffer(server_responce), delimiter, use_awaitable
+    );
+
+    auto content_lenght = findContentLength(server_response);
+
+    if(!content_lenght.has_value())
+    {
+      char data[8192];
+
+      while(true)
+      {
+        error_code ec;
+
+        std::size_t bytes_read = co_await server_socket.async_read_some(
+          buffer(data), boost::asio::redirect_errors(use_awaitable, ec)
+        );
+
+        if(ec == boost::asio::error::eof)
+        {
+          break;
+        }
+
+        if(ec)
+        {
+          throw boost::system::system_error(ec);
+        }
+
+        co_await boost::asio::async_write(
+          client_socket, buffer(data, bytes_read), use_awaitable
+        );
+      }
+    }
+    else
+    {
+      std::size_t header_end = server_response.find(std::string(delimiter));
+
+      if(header_end == std::string::npos)
+      {
+        throw std::runtime_error{"Invalid http response"};
+      }
+
+      header_end += delimiter.size();
+
+      std::size_t already_have_body = server_response.size() - header_end;
+
+      std::size_t remaining = 0;
+      if(*content_lenght > already_have_body)
+      {
+        remaining = *content_lenght - already_have_body;
+      }
+
+      char data[8192];
+      
+      while(remaining > 0)
+      {
+        std::size_t bytes_to_read = std::min<std::size_t>(
+          sizeof(data);
+          remaining;
+        );
+
+        std::size_t bytes_read = co_await server_socket.async_read_some(
+          buffer(data, bytes_to_read), use_awaitable
+        );
+
+        remaining -= bytes_read;
+
+        co_await boost::asio::async_write(
+          client_socket, buffer(data, bytes_read), use_awaitable
+        );
+      }
+    }
+
+    client_socket.close();
+    server_socket.close();
+
+  }
+  catch(const std::excecption& e)
+  {
+    std::cerr << "Session error " << e.what() << std::endl;
+  }
 }
 
 class Server
@@ -42,33 +161,55 @@ public:
   }
 
 private:
-  void do_accept()
-  {
-    acceptor_.async_accept(socket_,
-      [this](error_code ec)
-      {
-        // code here
-      }
-    );
-  }
+    void do_accept() {
+        acceptor_.async_accept(
+            socket_,
+            [this](error_code ec) {
+                if (!ec) 
+                {
+                    std::cout << "Accepted new connection" << std::endl;
 
+                    co_spawn(io_service_, session(std::move(socket_), io_service_), detached);
+
+                    socket_ = tcp::socket(io_service_);
+                } 
+                else 
+                {
+                    std::cerr << "Accept error: " << ec.message() << std::endl;
+                }
+
+                do_accept();
+            }
+        );
+    }
+
+private:
   io_service& io_service_;
   tcp::acceptor acceptor_;
   tcp::socket socket_;
 };
 
 int main(int argc, char* argv[]) {
-  try {
-    if (argc != 2) {
-      std::cerr << "Usage: proxy_server";
-      std::cerr << " <listen_port>\n";
+    try 
+    {
+      if (argc != 2) 
+      {
+          std::cerr << "Usage: proxy_server <listen_port>\n";
+          return 1;
+      }
+
+      io_service io_service(1);
+
+      Server server(io_service, static_cast<short>(std::atoi(argv[1])));
+
+      io_service.run();
+
+    } 
+    catch (const std::exception& e) 
+    {
+      std::cerr << "Exception: " << e.what() << std::endl;
       return 1;
     }
-    io_service io_service(1);
-    Server server(io_service, std::atoi(argv[1]));
-    io_service.run();
 
-  } catch (const std::exception& e) {
-    std::cerr << "Exception: " << e.what() << std::endl;
-  }
+    return 0;
 }
